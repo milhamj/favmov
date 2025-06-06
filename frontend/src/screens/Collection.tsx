8a18d8bda606b323e6e7e891e3042ae5bb882c66
milhamj <ilham.jamaludin@bytedance.com>
2025-06-06 17:54:29 +0700

    WIP Collection page :construction:

--- frontend/src/screens/Collection.tsx
+++ frontend/src/screens/Collection.tsx
@@ -5,30 +5,38 @@ import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/navigationTypes';
 import withAuth from '../components/withAuth';
+import Toast from 'react-native-toast-message';
+import PageContainer from '../components/PageContainer';
+import TopBar from '../components/TopBar';
 
 
 const Collection = withAuth(() => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-
-  // Placeholder data for collections
+  
   const collections = [
     { id: '1', name: 'Default Collection', movies: [] },
     // Add more collections as needed
   ];
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={collections}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View>
-            <Text style={styles.collectionName}>{item.name}</Text>
-            {/* Render movies in the collection */}
-          </View>
-        )}
+    <PageContainer>
+      <TopBar
+        title= { 'Collection' }
       />
-    </View>
+      <View style={styles.container}>
+        <FlatList
+          data={collections}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View>
+              <Text style={styles.collectionName}>{item.name}</Text>
+              {/* Render movies in the collection */}
+            </View>
+          )}
+        />
+      </View>
+      <Toast />
+    </PageContainer>
   );
 });
 
