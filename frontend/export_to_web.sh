8b8646c046907c733a9faa818bf9f6edad8c3d30
milhamj <ilham.jamaludin@bytedance.com>
2025-10-03 16:41:08 +0700

    IT FREAKING WORKS IN BE :fire::fire::fire:

--- frontend/export_to_web.sh
+++ frontend/export_to_web.sh
@@ -1,9 +1,13 @@
+cat '[0] Clearing ./dist folder'
 rm -rf ./dist
 
+cat '[1] Building web version'
 pnpm exec expo export -p web
 
+cat '[2] Clearing our BE public folder'
 rm -rf ../backend/public/*
 
 sleep 1
 
+cat '[3] Copying files to BE public folder'
 mv ./dist/* ../backend/public/
