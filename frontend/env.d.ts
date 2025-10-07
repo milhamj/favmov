d0739e05938343ddf791429b9735205f2217b19f
milhamj <ilham.jamaludin@bytedance.com>
2025-10-07 20:26:13 +0700

    Update env variables :wrench:

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,7 +1,6 @@
 declare module '@env' {
-  export const FAVMOV_NODE_ENV: string;
   export const FAVMOV_TMDB_API_KEY: string;
   export const FAVMOV_SUPABASE_URL: string;
   export const FAVMOV_SUPABASE_ANON_KEY: string;
-  export const FAVMOV_API_BASE_URL: string;
+  export const FAVMOV_API_BASE_URL_PROD: string;
 }
--- frontend/src/utils/util.ts
+++ frontend/src/utils/util.ts
@@ -1,13 +1,3 @@
-import { FAVMOV_NODE_ENV } from '@env';
-
-/**
- * Checks if the current environment is development
- * @returns {boolean} True if the environment is development, false otherwise
- */
-export const isDevelopment = (): boolean => {
-  return FAVMOV_NODE_ENV === 'development';
-};
-
 export const parseStrParam = (param: string | string[]): string => {
   return Array.isArray(param) ? param[0] : param;
 }
