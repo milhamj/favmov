5c783dc5495b1f1020db4470af6d232f9ec99bb8
milhamj <ilham.jamaludin@bytedance.com>
2025-10-17 20:40:05 +0700

    Fix not having seo metas :bug:

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -2,7 +2,6 @@ import { Stack } from 'expo-router';
 import Toast from 'react-native-toast-message';
 import { SeoHead } from '../components/SeoHead';
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../services/supabaseClient';
 import { ActivityIndicator } from 'react-native';
 
 const RootLayout = () => {
@@ -12,24 +11,27 @@ const RootLayout = () => {
     setReady(true)
   }, [])
 
-  if (!ready) return <ActivityIndicator style={{ flex: 1 }} size="large" color="tomato"/>
-
   return (
     <>
       <SeoHead/>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="search" />
-        <Stack.Screen name="movie/[id]" />
-        <Stack.Screen name="collection/[id]" />
-        <Stack.Screen name="collection/add" />
-        <Stack.Screen name="login" />
-      </Stack>
-      <Toast />
+      { ready ? 
+        <>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="search" />
+            <Stack.Screen name="movie/[id]" />
+            <Stack.Screen name="collection/[id]" />
+            <Stack.Screen name="collection/add" />
+            <Stack.Screen name="login" />
+          </Stack>
+          <Toast />
+        </>
+        : <ActivityIndicator size="large" color="tomato" style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} />
+      }
     </>
   );
 }
